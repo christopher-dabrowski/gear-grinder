c43911905868686cf6524991fb44da45322e68bd
Krzysztof Dąbrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2025-10-29 18:04:47 +0100

    feat: code to dest different right single wheel delays

---
+++ src/class1/test_wheel_delay.ino
@@ -0,0 +1,67 @@
+#define SINGLE_RIDE_TIME_MS 500
+#define LED_BUILTIN_1 2
+#define LED_BUILTIN_2 16
+
+void setup() {
+  pinMode(D1, OUTPUT); // konfigurujemy pin wł i wył prawego silnika
+  pinMode(D2, OUTPUT); // konfigurujemy pin wł i wył lewego silnika
+  pinMode(D3, OUTPUT); // konfigurujemy pin wyboru kierunku prawego silnika
+  pinMode(D4, OUTPUT); // konfigurujemy pin wyboru kierunku lewego silnika
+}
+
+void displayLedValue(int value) {
+  if (value == 0) {
+    digitalWrite(LED_BUILTIN_1, LOW);
+    digitalWrite(LED_BUILTIN_2, LOW);
+  } else if (value == 1) {
+    digitalWrite(LED_BUILTIN_1, HIGH);
+    digitalWrite(LED_BUILTIN_2, LOW);
+  } else if (value == 2) {
+    digitalWrite(LED_BUILTIN_1, LOW);
+    digitalWrite(LED_BUILTIN_2, HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN_1, HIGH);
+    digitalWrite(LED_BUILTIN_2, HIGH);
+  }
+}
+
+void rideForward(int rightDelayMs = 50) {
+  digitalWrite(D3, HIGH); // kierunek prawy do przodu
+  digitalWrite(D4, HIGH); // kierunek lewy do przodu
+  digitalWrite(D2, HIGH); // JAZDA - właczamy lewy silnik
+  digitalWrite(D1, HIGH); // JAZDA - właczamy prawy silnik
+
+  delay(SINGLE_RIDE_TIME_MS);
+  digitalWrite(D2, LOW); // STOP - wyłaczamy lewy silnik
+  // The robot sways to the right so the right wheel needs to work a bit longer
+  // in each cycle
+  delay(rightDelayMs);
+  digitalWrite(D1, LOW); // STOP - wyłaczamy prawy silnik
+}
+
+void rideBackward(int leftDelayMs = 50) {
+  digitalWrite(D3, LOW);  // kierunek prawy do tyłu
+  digitalWrite(D4, LOW);  // kierunek lewy do tyłu
+  digitalWrite(D2, HIGH); // JAZDA - właczamy lewy silnik
+  digitalWrite(D1, HIGH); // JAZDA - właczamy prawy silnik
+
+  delay(SINGLE_RIDE_TIME_MS);
+  digitalWrite(D1, LOW); // STOP - wyłaczamy prawy silnik
+  // When going backward, the robot sways to the left, so the left wheel needs
+  // to work a bit longer
+  delay(leftDelayMs);
+  digitalWrite(D2, LOW); // STOP - wyłaczamy lewy silnik
+}
+
+void loop() {
+  int delaysToTest[] = {0, 50, 75, 100};
+
+  for (int i = 0; i < sizeof(delaysToTest) / sizeof(delaysToTest[0]); i++) {
+    int delayMs = delaysToTest[i];
+    displayLedValue(i);
+    for (int i = 0; i < 3; i++) {
+      rideForward(delayMs);
+    }
+    delay(1000);
+  }
+}
