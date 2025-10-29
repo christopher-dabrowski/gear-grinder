517b37fc28fa5ca469a0278de5fee026e48c5a56
Krzysztof Dąbrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2025-10-29 20:34:38 +0100

    feat: code changes from class

--- src/class1/test_wheel_delay/test_wheel_delay.ino
+++ src/class1/test_wheel_delay/test_wheel_delay.ino
@@ -3,6 +3,9 @@
 #define LED_BUILTIN_2 16
 
 void setup() {
+  pinMode(LED_BUILTIN_1, OUTPUT);
+  pinMode(LED_BUILTIN_2, OUTPUT);
+
   pinMode(D1, OUTPUT); // konfigurujemy pin wł i wył prawego silnika
   pinMode(D2, OUTPUT); // konfigurujemy pin wł i wył lewego silnika
   pinMode(D3, OUTPUT); // konfigurujemy pin wyboru kierunku prawego silnika
@@ -11,8 +14,8 @@ void setup() {
 
 void displayLedValue(int value) {
   if (value == 0) {
-    digitalWrite(LED_BUILTIN_1, LOW);
-    digitalWrite(LED_BUILTIN_2, LOW);
+    digitalWrite(LED_BUILTIN_1, HIGH);
+    digitalWrite(LED_BUILTIN_2, HIGH);
   } else if (value == 1) {
     digitalWrite(LED_BUILTIN_1, HIGH);
     digitalWrite(LED_BUILTIN_2, LOW);
@@ -20,9 +23,18 @@ void displayLedValue(int value) {
     digitalWrite(LED_BUILTIN_1, LOW);
     digitalWrite(LED_BUILTIN_2, HIGH);
   } else {
-    digitalWrite(LED_BUILTIN_1, HIGH);
-    digitalWrite(LED_BUILTIN_2, HIGH);
+    digitalWrite(LED_BUILTIN_1, LOW);
+    digitalWrite(LED_BUILTIN_2, LOW);
   }
+}
+
+void runBothEngines(int runTimeMs) {
+  digitalWrite(D2, HIGH); // JAZDA - właczamy lewy silnik
+  digitalWrite(D1, HIGH); // JAZDA - właczamy prawy silnik
+
+  delay(runTimeMs);
+  digitalWrite(D1, LOW); // STOP - wyłaczamy prawy silnik
+  digitalWrite(D2, LOW); // STOP - wyłaczamy lewy silnik
 }
 
 void rideForward(int rightDelayMs = 50) {
@@ -53,15 +65,58 @@ void rideBackward(int leftDelayMs = 50) {
   digitalWrite(D2, LOW); // STOP - wyłaczamy lewy silnik
 }
 
-void loop() {
-  int delaysToTest[] = {0, 50, 75, 100};
+void rotateClockwise(int rotateTime) {
+  digitalWrite(D3, HIGH); // kierunek prawy do przodu
+  digitalWrite(D4, LOW);  // kierunek lewy do tyłu
+  digitalWrite(D2, HIGH); // JAZDA - właczamy lewy silnik
+  digitalWrite(D1, HIGH); // JAZDA - właczamy prawy silnik
 
-  for (int i = 0; i < sizeof(delaysToTest) / sizeof(delaysToTest[0]); i++) {
-    int delayMs = delaysToTest[i];
-    displayLedValue(i);
-    for (int i = 0; i < 3; i++) {
-      rideForward(delayMs);
-    }
-    delay(1000);
+  delay(rotateTime);
+  digitalWrite(D1, LOW); // STOP - wyłaczamy prawy silnik
+  digitalWrite(D2, LOW); // STOP - wyłaczamy lewy silnik
+}
+
+void rotateCounterClockwise(int rotateTime) {
+  digitalWrite(D3, LOW);  // kierunek prawy do tyłu
+  digitalWrite(D4, HIGH); // kierunek lewy do przodu
+  digitalWrite(D2, HIGH); // JAZDA - właczamy lewy silnik
+  digitalWrite(D1, HIGH); // JAZDA - właczamy prawy silnik
+
+  delay(rotateTime);
+  digitalWrite(D1, LOW); // STOP - wyłaczamy prawy silnik
+  digitalWrite(D2, LOW); // STOP - wyłaczamy lewy silnik
+}
+
+bool done = false;
+
+void loop() {
+  // if (done) {
+  //   delay(1000);
+  //   return;
+  // }
+
+  for (int i = 0; i < 5; i++) {
+    rideForward(75);
   }
+
+  rotateClockwise(380);
+
+  for (int i = 0; i < 5; i++) {
+    rideForward(75);
+  }
+
+  rotateClockwise(380);
+
+  // done = true;
+
+  // int delaysToTest[] = {0, 50, 75, 100};
+
+  // for (int i = 0; i < sizeof(delaysToTest) / sizeof(delaysToTest[0]); i++) {
+  //   int delayMs = delaysToTest[i];
+  //   displayLedValue(i);
+  //   for (int i = 0; i < 3; i++) {
+  //     rideForward(delayMs);
+  //   }
+  //   delay(3000);
+  // }
 }
